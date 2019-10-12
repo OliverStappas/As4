a8eed7cae728992e4e7fd312b3b5fc5aa7981e28
Oliver Stappas <o.stappas@gmail.com>
2019-10-11 23:51:44 -0400

    Just need photos

--- src/aBall.java
+++ src/aBall.java
@@ -1,19 +1,22 @@
+// Comments with *** in front them are taken from the ECSE 202 Assignment 1 and 2 Instructions
+
 import acm.graphics.GOval;
 import java.awt.*;
 
 public class aBall extends Thread  {
 
     private static final int WIDTH = 1200; // n.b. screen coordinates
-    private static final int HEIGHT = 600;
-    private static final int OFFSET = 200;
-    private static final double SCALE = HEIGHT/100; // pixels per meter
-    private static final double g = 9.8;               // MKS gravitational constant 9.8 m/s^2
-    private static final double Pi = 3.141592654;      // To convert degrees to radians
-    private static final double k = 0.0016;            // Air resistance parameter
-    private static final double ETHR = 0.01;           // If either Vx or Vy < ETHR STOP
-    private static final double PD = 1;                // Trace point diameter
+    private static final int HEIGHT = 600; //***
+    private static final int OFFSET = 200; //***
+    private static final double SCALE = HEIGHT/100;    // pixels per meter  //***
+    private static final double g = 9.8;               // MKS gravitational constant 9.8 m/s^2 //***
+    private static final double Pi = 3.141592654;      // To convert degrees to radians //***
+    //private static final double k = 0.0001;            // Air resistance parameter //***
+    private static final double k = 0.0016;           // Air resistance parameter for red ball test //***
+    private static final double ETHR = 0.01;           // Energy threshold //***
+    private static final double PD = 1;                // Trace point diameter //***
     // Initializing variables
-    double TICK = 0.1;
+    double TICK = 0.1; //***
     double Xi = 0;
     double Yi = 0;
     double Vo = 0;
@@ -22,6 +25,9 @@ public class aBall extends Thread  {
     Color bColor;
     double bLoss = 0;
     GOval myBall;
+    private bSim link;
+
+
 
     /**
      * The constructor specifies the parameters for simulation. They are
@@ -36,25 +42,28 @@ public class aBall extends Thread  {
      */
 
     public aBall (double Xi, double Yi, double Vo, double theta,
-                  double bSize, Color bColor, double bLoss) {
-
-        this.Xi = Xi; // Get simulation parameters
-        this.Yi = Yi;
-        this.Vo = Vo;
-        this.theta = theta;
-        this.bSize = bSize/2;
-        this.bColor = bColor;
-        this.bLoss = bLoss;
+                  double bSize, Color bColor, double bLoss, bSim link) {
+        // Get simulation parameters
+        this.Xi = Xi; //***
+        this.Yi = Yi; //***
+        this.Vo = Vo; //***
+        this.theta = theta; //***
+        this.bSize = bSize; //***
+        this.bColor = bColor; //***
+        this.bLoss = bLoss; //***
 
         // Creating a ball from constructor parameters
-        double bPixelSize = this.bSize * SCALE;
+        double bPixelSize = this.bSize * SCALE; //***
         myBall = new GOval(this.Xi * SCALE, this.Yi * SCALE, bPixelSize * 2, bPixelSize * 2);
         myBall.setFilled(true);
         myBall.setColor(this.bColor);
 
+        this.link = link;
+
     }
-    public GOval getBall() {
-        return myBall;
+
+    public GOval getBall() { //***
+        return myBall; //***
     }
 
     /**
@@ -68,9 +77,9 @@ public class aBall extends Thread  {
 
     public void run() {
 // Simulation goes here...
-        double Vt = g / (4 * Pi * this.bSize * this.bSize * k); // Terminal velocity
-        double Vox = this.Vo * Math.cos(theta * Pi / 180); // Initial x velocity
-        double Voy = this.Vo * Math.sin(theta * Pi / 180); // Initial y velocity
+        double Vt = g / (4 * Pi * this.bSize * this.bSize * k); // Terminal velocity //***
+        double Vox = this.Vo * Math.cos(theta * Pi / 180); // Initial x velocity //***
+        double Voy = this.Vo * Math.sin(theta * Pi / 180); // Initial y velocity //***
         double Xlast = 0; // Previous X position
         double Ylast = 0; // Previous Y position
         double time = 0; // Initial time
@@ -92,18 +101,18 @@ public class aBall extends Thread  {
         // Simulation loop
         while (running) {
             // Update parameters
-            double X = Vox * Vt / g * (1 - Math.exp(-g * time / Vt)) + Xo; // X position
-            double Y = Vt / g * (Voy + Vt) * (1 - Math.exp(-g * time / Vt)) - Vt * time + yOffset; // Y position
-            double Vx = (X - Xlast) / TICK; // Estimate Vx from difference
-            double Vy = (Y - Ylast) / TICK; // Estimate Vy from difference
+            double X = Vox * Vt / g * (1 - Math.exp(-g * time / Vt)) + Xo; // X position //***
+            double Y = Vt / g * (Voy + Vt) * (1 - Math.exp(-g * time / Vt)) - Vt * time + yOffset; // Y position //***
+            double Vx = (X - Xlast) / TICK; // Estimate Vx from difference //***
+            double Vy = (Y - Ylast) / TICK; // Estimate Vy from difference //***
 
             // Detecting collision with ground
             if (Vy < 0 && Y <= this.bSize) {
-                double KEx = 0.5 * Vx * Vx * (1 - this.bLoss); // Kinetic energy in X direction after collision
-                double KEy = 0.5 * Vy * Vy * (1 - this.bLoss); // Kinetic energy in y direction after collision
+                double KEx = 0.5 * Vx * Vx * (1 - this.bLoss); // Kinetic energy in X direction after collision //***
+                double KEy = 0.5 * Vy * Vy * (1 - this.bLoss); // Kinetic energy in y direction after collision //***
 
-                Vox = sign * Math.sqrt(2 * KEx); // Resulting horizontal velocity
-                Voy = Math.sqrt(2 * KEy); // Resulting vertical velocity
+                Vox = sign * Math.sqrt(2 * KEx); // Resulting horizontal velocity //***
+                Voy = Math.sqrt(2 * KEy); // Resulting vertical velocity //***
 
                 // Reset variables
                 Xo = X;
@@ -124,28 +133,30 @@ public class aBall extends Thread  {
             }
 
             // Display update (Screen coordinates)
-            int ScrX = (int) ((X - bSize) * SCALE);
-            int ScrY = (int) (HEIGHT - (Y + bSize) * SCALE);
+            int ScrX = (int) ((X - bSize) * SCALE); //***
+            int ScrY = (int) (HEIGHT - (Y + bSize) * SCALE); //***
 
             // The current values of X and Y will be the previous positions for the next loop
             Xlast = X;
             Ylast = Y;
 
             // Moving red ball and drawing trace points
-            myBall.setLocation(ScrX, ScrY);  // Moving the red ball to the desired screen coordinates
-            //GOval tracePoint = new GOval(X * SCALE, HEIGHT - Y * SCALE, PD, PD); // Initializing the tracepoints
-            //tracePoint.setFilled(true);
-            //add(tracePoint); // Drawing the tracepoints on the screen
+            myBall.setLocation(ScrX, ScrY); //***  // Moving the red ball to the desired screen coordinates
+            if (link != null) { //***
+                GOval tracePoint = new GOval(X * SCALE, HEIGHT - Y * SCALE, PD, PD); // Initializing the tracepoints
+                tracePoint.setFilled(true);
+                link.add(tracePoint); // Drawing the tracepoints on the screen
+            }
 
             // Time Update
             time += TICK;
 
             // Animation delay
 
-            try { // pause for 50 milliseconds
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            try { // pause for 50 milliseconds  //***
+                Thread.sleep(50);            //***
+            } catch (InterruptedException e) {  //***
+                e.printStackTrace();            //***
             }
         }
     }
--- src/bSim.java
+++ src/bSim.java
@@ -3,26 +3,27 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 import java.awt.*;
 
+
 public class bSim extends GraphicsProgram {
     // Parameters used in this program
-    private static final int WIDTH = 1200; // n.b. screen coordinates
-    private static final int HEIGHT = 600;
-    private static final int OFFSET = 200;
-    private static final double SCALE = HEIGHT/100; // pixels per meter
-    private static final int NUMBALLS = 100; // # balls to simulate
-    private static final double MINSIZE = 1.0; // Minumum ball radius (meters)
-    private static final double MAXSIZE = 10.0; // Maximum ball radius (meters)
-    private static final double EMIN = 0.1; // Minimum loss coefficient
-    private static final double EMAX = 0.6; // Maximum loss coefficient
-    private static final double VoMIN = 40.0; // Minimum velocity (meters/sec)
-    private static final double VoMAX = 50.0; // Maximum velocity (meters/sec)
-    private static final double ThetaMIN = 80.0; // Minimum launch angle (degrees)
-    private static final double ThetaMAX = 100.0; // Maximum launch angle (degrees)
-    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private static final int WIDTH = 1200; //*** n.b. screen coordinates
+    private static final int HEIGHT = 600; //***
+    private static final int OFFSET = 200; //***
+    private static final double SCALE = HEIGHT/100; // pixels per meter //***
+    private static final int NUMBALLS = 100; // # balls to simulate //***
+    private static final double MINSIZE = 1.0; // Minimum ball radius (meters ) //***
+    private static final double MAXSIZE = 10.0; // Maximum ball radius (meters) //***
+    private static final double EMIN = 0.1; // Minimum loss coefficient //***
+    private static final double EMAX = 0.6; // Maximum loss coefficient //***
+    private static final double VoMIN = 40.0; // Minimum velocity (meters/sec) //***
+    private static final double VoMAX = 50.0; // Maximum velocity (meters/sec) //***
+    private static final double ThetaMIN = 80.0; // Minimum launch angle (degrees) //***
+    private static final double ThetaMAX = 100.0; // Maximum launch angle (degrees) //***
+    private RandomGenerator rgen = RandomGenerator.getInstance(); //***
 
 
     public void run() {
-        this.resize(WIDTH, HEIGHT + OFFSET); // size display window
+        this.resize(WIDTH, HEIGHT + OFFSET); //*** size display window
 
         // Ground plane
         GRect rect = new GRect(0, HEIGHT, 1200, 3);
@@ -30,35 +31,31 @@ public class bSim extends GraphicsProgram {
         add(rect);
 
         // Set seed for randomness
-        rgen.setSeed((long) 0.12345);
+        rgen.setSeed((long) 0.12345); //***
 
         // for loop to randomize and create 100 different balls
-        for (int i = 1; i <= 100; i++) {
-            // Randomizing the different aBall parameters with boundaries
-            double Vo = rgen.nextDouble(VoMIN, VoMAX);
-            double theta = rgen.nextDouble(ThetaMIN, ThetaMAX);
-            double bSize = rgen.nextDouble(MINSIZE, MAXSIZE);
-            Color bColor = rgen.nextColor();
-            double bLoss = rgen.nextDouble(EMIN, EMAX);
-
-            // Creating the ball with the previously randomly generate paramters
-            aBall ball = new aBall((WIDTH/2)/SCALE,bSize,Vo,theta,bSize,bColor,bLoss);
-            add(ball.getBall());
-            ball.start();
-
-        }
+//        for (int i = 1; i <= 100; i++) {
+//            // Randomizing the different aBall parameters with boundaries
+//            double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); //***
+//            Color bColor = rgen.nextColor(); //***
+//            double bLoss = rgen.nextDouble(EMIN, EMAX); //***
+//            double bVel = rgen.nextDouble(VoMIN, VoMAX); //***
+//            double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); //***
+//
+//            // Creating the ball with the previously randomly generate parameters
+//            aBall ball = new aBall((WIDTH/2)/SCALE,bSize,bVel,theta,bSize,bColor,bLoss,null); // Adding the ball (add
+//                                                                                                       // link instead of null if
+//                                                                                                       // you want tracepoints)
+//            add(ball.getBall());
+//            ball.start();
+//
+//        }
 
         // Test for one ball
-        //aBall redBall = new aBall(10.0,100.0,1.0,30.0,6.0,Color.RED,0.25);
-        //aBall redBall = new aBall(5,1,40,85,1,Color.RED,0.4);
-        //add(redBall.getBall());
-        //redBall.start();
+       // aBall redBall = new aBall(10.0,100.0,1.0,30.0,6.0,Color.RED,0.25,this); //***
+        aBall redBall = new aBall(5,1,40,85,1,Color.RED,0.4,this); //***
+        add(redBall.getBall()); //***
+        redBall.start(); //***
 
-        // For example, to generate a random loss parameter, one would use an
-        // instance of the RandomGenerator class as follows:
-        //double iLoss = rgen.nextDouble(EMIN ,EMAX );
     }
 }
-
-//            println("Ball "+ i + " Vo: " + Vo + " theta: " + theta + " bSize: " + bSize + " bColor: " + bColor
-//                    + " bLoss: " + bLoss);
