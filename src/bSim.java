f52cde937b52d1e5e4c3a790406a422201f4f795
Oliver Stappas <o.stappas@gmail.com>
2019-11-06 00:14:48 -0500

    Garbage

--- src/bSim.java
+++ src/bSim.java
@@ -1,7 +1,8 @@
 // Comments with *** in front them are taken from the ECSE 202 Assignment 1, 2 and 3 Instructions
 
 import acm.graphics.*;
-import acm.program.*;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 import java.awt.*;
 import java.awt.event.*;
@@ -30,127 +31,138 @@ public class bSim extends GraphicsProgram {
     private static final double ThetaMIN = 80.0; // Minimum launch angle (degrees) //***
     private static final double ThetaMAX = 100.0; // Maximum launch angle (degrees) //***
     private RandomGenerator rgen = RandomGenerator.getInstance(); //***
+    boolean simEnable = false;
 
-    //public void doSim(){}
-    //public void doStack(){}
+    public void init() {
+        //…other code…
+        setLayout(new TableLayout(2, 3));
+        fahrenheitField = new IntField(32);
+        fahrenheitField.setActionCommand("F -> C");
+        fahrenheitField.addActionListener(this);
+        celsiusField = new IntField(0);
+        celsiusField.setActionCommand("C -> F");
+        celsiusField.addActionListener(this);
+        add((new JLabel("Degrees Fahrenheit")),NORTH);
+        add(fahrenheitField);
+        add(new JButton("F -> C"));
+        add(new JLabel("Degrees Celsius"));
+        add(celsiusField);
+        add(new JButton("C -> F"));
+        addActionListeners();
 
-    //add(new JButton("Run"));
-    //add(new JButton("Stack"));
-    //add(new JButton("Clear"));
-    //add(new JButton("Stop"));
-    //add(new JButton("Quit"));
-
-
-    //String[] petStrings = { "Run", "Stack", "Clear", "Stop", "Quit" };
-
-    //Create the combo box, select item at index 4.
-    //Indices start at 0, so 4 specifies the pig.
-    //JComboBox petList = new JComboBox(petStrings);
-    //petList.setSelectedIndex(4);
-    //petList.addActionListener(this);
-
-    //JComboBox sizeChooser = new JComboBox();
-    //add(new JComboBox(""), NORTH);
-    //JToggleButton
-    //sizeChooser.addItem("Small");
-    //sizeChooser.addItem("Medium");
-    //sizeChooser.addItem("Large");
-    //sizeChooser.addItem("X-Large");
-    //sizeChooser.setEditable(false);
-
-    //new JSlider (min, max, value)
-
-    /**
-     * This program allows users to convert temperatures back and forth
-     * from Fahrenheit to Celsius.
-     */
-    public class TemperatureConverter extends Program {
-        /* Initializes the graphical user interface */
-        public void init() {
-
-            setLayout(new TableLayout(2, 3));
-            fahrenheitField = new IntField(32);
-            fahrenheitField.setActionCommand("F -> C");
-            fahrenheitField.addActionListener(this);
-            celsiusField = new IntField(0);
-            celsiusField.setActionCommand("C -> F");
-            celsiusField.addActionListener(this);
-            add((new JLabel("Degrees Fahrenheit")),NORTH);
-            add(fahrenheitField);
-            add(new JButton("F -> C"));
-            add(new JLabel("Degrees Celsius"));
-            add(celsiusField);
-            add(new JButton("C -> F"));
-            addActionListeners();
-        }
-
-        /* Listens for a button action */
-        public void actionPerformed(ActionEvent e) {
-            String cmd = e.getActionCommand();
-            if (cmd.equals("F -> C")) {
-                int f = fahrenheitField.getValue();
-                int c = GMath.round((5.0 / 9.0) * (f - 32));
-                celsiusField.setValue(c);
-            } else if (cmd.equals("C -> F")) {
-                int c = celsiusField.getValue();
-                int f = GMath.round((9.0 / 5.0) * c + 32);
-                fahrenheitField.setValue(f);
+            while(true) {
+                pause(200);
+                if (simEnable) { // Run once, then stop
+                    doSim();
+                    bSimC.setSelectedIndex(0);
+                    simEnable = false;
+                }
             }
-        }
-        /* Private instance variables */
-        private IntField fahrenheitField;
-        private IntField celsiusField;
+
     }
 
 
 
-//    public void run() {
-//        this.resize(WIDTH, HEIGHT + OFFSET); //*** size display window
-//
-//        // Ground plane
-//        GRect rect = new GRect(0, HEIGHT, 1200, 3);
-//        rect.setFilled(true);
-//        add(rect);
-//
-//        // Creating instance of bTree class
-//        bTree myTree = new bTree(); //***
-//
-//        // Set seed for randomness
-//        rgen.setSeed((long) 424242); //***
-//
-//
-//
-//        // for loop to randomize and create 60 different balls
-//        for (int i = 1; i <= NUMBALLS; i++) {
-//            // Randomizing the different aBall parameters with boundaries
-//            double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); //***
-//            Color bColor = rgen.nextColor(); //***
-//            double bLoss = rgen.nextDouble(EMIN, EMAX); //***
-//            double bVel = rgen.nextDouble(VoMIN, VoMAX); //***
-//            double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); //***
-//
-//            // Creating the ball with the previously randomly generate parameters
-//            aBall iBall = new aBall((WIDTH/2)/SCALE,bSize,bVel,theta,bSize,bColor,bLoss,this); // Adding the ball // Null for no trace, this for trace
-//            add(iBall.getBall()); //*** Getting the ball object
-//            myTree.addNode(iBall); //*** Adding balls to a bTree
-//            iBall.start(); // Starting the ball simulation
-//
-//        }
-//
-//        while (myTree.isRunning()) {} //*** Loop to block following code from running
-//        GLabel label1 = new GLabel("Click mouse to continue", WIDTH/2, HEIGHT/2); // Message to click to stack balls
-//        label1.setFont("SansSerif-24");
-//        label1.setColor(Color.RED);
-//        add(label1);
-//        //Code to wait for a mouse click // Wait
-//        this.waitForClick(); // Wait for user to click to continue program (found from looking through Glabel methods)
-//        label1.setVisible(false); // Making the first message invisible (found from looking through Glabel methods)
-//        myTree.stackBalls(); //*** Lay out balls in order
-//        GLabel label2 = new GLabel("All Stacked!", WIDTH/2, HEIGHT/2); // Message that balls are stacked
-//        label2.setFont("SansSerif-36");
-//        label2.setColor(Color.RED);
-//        add(label2);
-//
-//
-//    }
+    /* Listens for a button action */
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        if (cmd.equals("F -> C")) {
+            int f = fahrenheitField.getValue();
+            int c = GMath.round((5.0 / 9.0) * (f - 32));
+            celsiusField.setValue(c);
+        } else if (cmd.equals("C -> F")) {
+            int c = celsiusField.getValue();
+            int f = GMath.round((9.0 / 5.0) * c + 32);
+            fahrenheitField.setValue(f);
+        }
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        JComboBox source = (JComboBox)e.getSource();
+        if (source==bSimC) { \
+            if (bSimC.getSelectedIndex()==1) {
+                System.out.println("Starting simulation");
+                doSim();
+            }
+            else if (bSimC.getSelectedIndex()==2) {
+                System.out.println("Histogramming balls");
+                doHist();
+            }
+            etc…
+        }
+    }
+
+    public void doSim() {
+        // Creating instance of bTree class
+        bTree myTree = new bTree(); //***
+
+        // for loop to randomize and create 60 different balls
+        for (int i = 1; i <= NUMBALLS; i++) {
+            // Randomizing the different aBall parameters with boundaries
+            double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); //***
+            Color bColor = rgen.nextColor(); //***
+            double bLoss = rgen.nextDouble(EMIN, EMAX); //***
+            double bVel = rgen.nextDouble(VoMIN, VoMAX); //***
+            double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); //***
+
+            // Creating the ball with the previously randomly generate parameters
+            aBall iBall = new aBall((WIDTH/2)/SCALE,bSize,bVel,theta,bSize,bColor,bLoss,this); // Adding the ball // Null for no trace, this for trace
+            add(iBall.getBall()); //*** Getting the ball object
+            myTree.addNode(iBall); //*** Adding balls to a bTree
+            iBall.start(); // Starting the ball simulation
+
+        }
+
+    }
+    /* Private instance variables */
+    private IntField fahrenheitField;
+    private IntField celsiusField;
+
+
+    public void run() {
+        this.resize(WIDTH, HEIGHT + OFFSET); //*** size display window
+
+        // Ground plane
+        GRect rect = new GRect(0, HEIGHT, 1200, 3);
+        rect.setFilled(true);
+        add(rect);
+
+        // Set seed for randomness
+        rgen.setSeed((long) 424242); //***
+
+
+
+        // for loop to randomize and create 60 different balls
+        for (int i = 1; i <= NUMBALLS; i++) {
+            // Randomizing the different aBall parameters with boundaries
+            double bSize = rgen.nextDouble(MINSIZE, MAXSIZE); //***
+            Color bColor = rgen.nextColor(); //***
+            double bLoss = rgen.nextDouble(EMIN, EMAX); //***
+            double bVel = rgen.nextDouble(VoMIN, VoMAX); //***
+            double theta = rgen.nextDouble(ThetaMIN, ThetaMAX); //***
+
+            // Creating the ball with the previously randomly generate parameters
+            aBall iBall = new aBall((WIDTH/2)/SCALE,bSize,bVel,theta,bSize,bColor,bLoss,this); // Adding the ball // Null for no trace, this for trace
+            add(iBall.getBall()); //*** Getting the ball object
+            myTree.addNode(iBall); //*** Adding balls to a bTree
+            iBall.start(); // Starting the ball simulation
+
+        }
+
+        while (myTree.isRunning()) {} //*** Loop to block following code from running
+        GLabel label1 = new GLabel("Click mouse to continue", WIDTH/2, HEIGHT/2); // Message to click to stack balls
+        label1.setFont("SansSerif-24");
+        label1.setColor(Color.RED);
+        add(label1);
+        //Code to wait for a mouse click // Wait
+        this.waitForClick(); // Wait for user to click to continue program (found from looking through Glabel methods)
+        label1.setVisible(false); // Making the first message invisible (found from looking through Glabel methods)
+        myTree.stackBalls(); //*** Lay out balls in order
+        GLabel label2 = new GLabel("All Stacked!", WIDTH/2, HEIGHT/2); // Message that balls are stacked
+        label2.setFont("SansSerif-36");
+        label2.setColor(Color.RED);
+        add(label2);
+
+
+    }
 }
